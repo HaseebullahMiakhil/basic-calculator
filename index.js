d43f85f432c29fb71c082f3f8ffc6261bf85b316
HaseebullahMiakhil <tw773029@gmail.com>
2026-05-16 13:59:07 +0430

    3

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ document.getElementById("num2-el").textContent = num2
 let sumEl = document.getElementById("sum-el")
 
 function add() {
-    sumEl.textContent = "Sum: " + num1 + num2
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
 }
 
